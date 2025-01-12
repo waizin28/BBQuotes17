7fe0e97cf4177126bd48aeb48d1fb2a8387a10f0
waizin28 <waizinlinn.08@gmail.com>
2025-01-12 14:25:36 -0600

    Initial Commit

---
+++ BBQuotes17/BBQuotes17App.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotes17App.swift
+//  BBQuotes17
+//
+//  Created by Wai Zin Linn on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotes17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
