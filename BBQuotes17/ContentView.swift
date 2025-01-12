4b16313d342f0a2921dc4a94d5daa5a475737001
waizin28 <waizinlinn.08@gmail.com>
2025-01-12 14:37:06 -0600

    added tab view to differentiate between breaking bad and better call saul views

--- BBQuotes17/ContentView.swift
+++ BBQuotes17/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView{
+            Text("Breaking Bad View")
+                .toolbarBackground(.visible, for: .tabBar) // to make tab differentator appear
+                .tabItem{
+                    Label("Breaking Bad", systemImage: "tortoise")
+                }
+            
+            Text("Better Call Saul View")
+                .toolbarBackground(.visible, for: .tabBar) // to make tab differentator appear
+                .tabItem{
+                    Label("Better Call Saul", systemImage: "briefcase")
+                }
         }
-        .padding()
+        .preferredColorScheme(.dark)
     }
 }
 
